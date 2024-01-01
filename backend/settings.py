e63c410bf7de1e71f793415f069175e43ecb44d0
HEMANT PATHAK <122811374+hemant030406@users.noreply.github.com>
2024-01-01 22:38:23 +0530

    new host added

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-$ghr4_(wjpb-d(=*n)otqjbql)=vxu@3tmv2!(@)9p07gikfs!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tictactoe-backend-utka.onrender.com']
 
 
 # Application definition
