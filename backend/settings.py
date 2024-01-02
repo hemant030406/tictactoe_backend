ed0eb3acd21dd2e5b2583e7caff2c101a83eff33
HEMANT PATHAK <122811374+hemant030406@users.noreply.github.com>
2024-01-02 11:32:32 +0530

    static files added

--- backend/settings.py
+++ backend/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = 'static/'
-STATIC_URL = 'static/'
+STATIC_URL = 'admin/'
 STATICFILES_DIRS = (
     '/var/www/static/my-dev',
 )
