9a4471f06570b16ec9864ae3bb6068e1836c4f3d
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-07-02 10:24:23 +0530

    added postgres

--- backend/settings.py
+++ backend/settings.py
@@ -71,16 +71,27 @@ ASGI_APPLICATION = 'backend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'JzsHAqkHvJTzQjPdsJcqHGglQvVcSnIL',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '40223',
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=100,unique = True)
+    username = models.CharField(max_length=100,unique = True)
     code = models.CharField(max_length=100)
     moves = models.JSONField(default=dict)
     scores = models.JSONField(default={
@@ -12,7 +12,5 @@ class User(AbstractUser):
         'X': 0
     })
     undo_stack = models.JSONField(default=list)
-    username = None
 
-    USERNAME_FIELD = 'name'
     REQUIRED_FIELDS = []
